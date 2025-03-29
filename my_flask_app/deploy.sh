dabd67917719f3a229ee649cb1f78d08ff489872
DILUSHA MUNASINGHE <dilushamunasinghe@DILUSHAs-MacBook-Air.local>
2025-03-29 22:12:25 +0530

    modified deploy.sh

--- my_flask_app/deploy.sh
+++ my_flask_app/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Navigate to the app directory
-cd /home/ubuntu/flaskapp-webhook/my_flask_app
-
-# Pull the latest changes from the repository
-git pull origin main
+cd /home/ubuntu/flaskapp-webhook/my_flask_app || exit
 
 # Activate the virtual environment
 source venv/bin/activate
+
+# Pull the latest changes from the repository
+git pull origin main
 
 # # Install any new dependencies
 # pip install -r requirements.txt
