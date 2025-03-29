0e93aac34ad7b5b9d5d47a4cd4eba4b5c7f6c1de
DILUSHA MUNASINGHE <dilushamunasinghe@DILUSHAs-MacBook-Air.local>
2025-03-29 13:12:36 +0530

    modified branch as main in deploy.sh

--- my_flask_app/deploy.sh
+++ my_flask_app/deploy.sh
@@ -4,7 +4,7 @@
 cd /home/ubuntu/flaskapp-webhook/my_flask_app
 
 # Pull the latest changes from the repository
-git pull origin master
+git pull origin main
 
 # Activate the virtual environment
 source venv/bin/activate
